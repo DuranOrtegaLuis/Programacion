94b66b0a08c727dbca54d11d9afa9a544a3fc6b2
LuisDuranOretega <62483624+LuisDuranOretega@users.noreply.github.com>
2020-06-27 03:32:59 -0500

    filas y columnas
    27-06-2020

---
+++ invertirfilas/Invertirfilas.java
@@ -0,0 +1,13 @@
+package invertirfilas;
+
+public class Invertirfilas {
+
+    public static void main(String[] args) {
+   
+       Matriz e1= new Matriz();
+       
+       e1.solicitar();
+       e1.hacer();
+    }
+    
+}
